39ac05a3c8a405c61e1b9ab7a1251cc3f999f798
Bailey Miller <bmiller@susquehannasoftware.com>
2017-06-30 13:31:56 -0400

    Getting this bad boy setup

--- CommunityTuber/src/app/app.module.ts
+++ CommunityTuber/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
